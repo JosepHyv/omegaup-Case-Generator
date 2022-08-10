b203d256d08a3a416d1794546c95b23fd700106f
JosepHyv <josephynimoto@gmail.com>
2022-08-10 15:13:12 -0500

    argparses finish

--- caseGen.py
+++ caseGen.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
+import shlex
 import pathlib
 import argparse
+import subprocess
 from sys import stderr, stdout
 #################
 
@@ -29,22 +31,27 @@ def cli() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        '-o', '--outputs',
-        action='store_true',
-        default=cwd(),
-        help='recibe la ruta de los casos de entrada, la ruta del ejecutable de solucion y la ruta de destino(por omision=CURRENT_WORKING_DIR) y genera solo los casos de salida'
+        '-v', '--version',
+        action='version',
+        version='%(prog)s 1.0.0'
     )
 
     parser.add_argument(
-        '-c', '--case-name',
-        action='store_true',
+        '-o', '--outputs',
+        action='store_true', #me permite guardar el input despues de la bandera
+        help='Nos permite generar solo las salidas dadas las entradas ya generadas, toma la ruta del generador de entradas como la ruta de todas las entradas almacenadas, la ruta del ejecutable de solucion y la ruta de destino(por omision=CURRENT_WORKING_DIR) y genera solo los casos de salida'
+    )
+
+    parser.add_argument(
+        '-c', '--case_name',
+        action='store',
         default=CASE_NAME,
         help="Recibe el nombre que llevaran los casos de entrada y salida (todos lo casos se nombraran como caseName#.in caseName#.out e.j casoFacil5.in casoFacil5.out)"
     )
 
     parser.add_argument(
-        '-t', '--total-cases',
-        dest="numero de casos",
+        '-t', '--total_cases',
+        action='store',
         type=int,
         default=10,
         help="denota el numero de casos que se generarán"
@@ -65,16 +72,25 @@ def cli() -> argparse.Namespace:
     parser.add_argument(
         'destinationFolder',
         type=pathlib.Path,
-        help="Dirección en donde se guardaran las input y outputs"
+        nargs='?',
+        default=cwd(),
+        help="Dirección en donde se guardaran las input y outputs (por omision=CURRENT_WORKING_DIR)"
     )
 
     return parser.parse_args()
-    
+
+def callExecs(inputGen : pathlib.Path, solGen : pathlib.Path, nameCase : str):
+    print(type(inputGen))
+    print(type(solGen))
+    print(nameCase)
+    pass
 
 def main():
     args = cli()
-    print(args)
+    
+    callExecs(args.sourceGenerator, args.sourceSolution, args.case_name)
 
+    print(args)
 
 if __name__ == "__main__":
     main()
